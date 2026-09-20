6f5585f92f294144c8bc302fef07229475d6c31c
jiek2889-art <308372594+jiek2889-art@users.noreply.github.com>
2026-09-20 22:18:02 +0800

    洛谷 P5737

--- jiek/0_4.py
+++ jiek/0_4.py
@@ -1,5 +1,6 @@
-﻿x = int(input("请输入一个整数: "))
-y = int(input("请输入一个整数: "))
+﻿a = input().split()
+x = int(a[0])
+y = int(a[1])
 num = []
 for i in range(x, y + 1):
     if i % 400 == 0 or (i % 4 == 0 and i % 100 != 0):
